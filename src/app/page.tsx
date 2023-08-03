5dfec403766dd29f998fc7f7ab2ce0ff191be471
nator <gowluonade@gmail.com>
2023-08-03 18:37:26 -0500

    grid_responsive-aspect-[9/14]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+'use client'
+import data from '../books.json'
+
+export interface Book {
+  title: string
+  pages: number
+  genre: string
+  cover: string
+  synopsis: string
+  year: number
+  ISBN: string
+}
+console.log('🚀 ~ file: page.tsx:3 ~ data:', data)
+const books: Book[] = data.library.map((data) => data.book)
 
 export default function Home () {
-  const a = ''
   return (
-    <main>
-      holis
-    </main>
+    <section className='grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4'>
+      {books.map((book) => (
+        <div key={book.ISBN}>
+          <img src={book.cover} alt={book.title} className='aspect-[9/14] object-cover'/>
+          <p>{book.title}</p>
+        </div>
+      ))}
+    </section>
   )
 }

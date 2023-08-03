ad6652d3b100300fb300463ee1c230595e509ec2
nator <gowluonade@gmail.com>
2023-08-03 18:53:00 -0500

    filter-manual

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from 'react'
 import data from '../books.json'
 
 export interface Book {
@@ -14,14 +15,32 @@ console.log('🚀 ~ file: page.tsx:3 ~ data:', data)
 const books: Book[] = data.library.map((data) => data.book)
 
 export default function Home () {
+  const [genre, setGenre] = useState<string>('')
+  const matches = genre !== ''
+    ? books.filter((book) => {
+      if (book.genre !== genre) return false
+
+      return true
+    })
+    : books
   return (
-    <section className='grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4'>
-      {books.map((book) => (
-        <div key={book.ISBN}>
-          <img src={book.cover} alt={book.title} className='aspect-[9/14] object-cover'/>
-          <p>{book.title}</p>
-        </div>
-      ))}
+    <section className='grid gap-4'>
+      <nav>
+        <select onChange={(e) => setGenre(e.target.value)}>
+          <option value="">Todos</option>
+          <option value="Fantasía">Fantasía</option>
+          <option value="Ciencia ficción">Ciencia ficción</option>
+        </select>
+      </nav>
+      <ul className='grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4'>
+        {matches.map((book) => (
+          <li key={book.ISBN}>
+            <img src={book.cover} alt={book.title} className='aspect-[9/14] object-cover'/>
+            <p>{book.title}</p>
+          </li>
+        ))}
+      </ul>
+
     </section>
   )
 }

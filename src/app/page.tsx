da1c23d5dff7b9ae9548c9d6315111c5ac2031db
nator <gowluonade@gmail.com>
2023-08-03 20:06:07 -0500

    dynamic import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Book } from '@/types'
-import data from '../books.json'
 import IndexClienPage from './client'
 
-const books: Book[] = data.library.map((data) => data.book)
-const genres: string[] = Array.from(new Set(books.map((book) => book.genre)))
+async function page () {
+  const books: Book[] = await import('../books.json').then((data) => data.library.map((data) => data.book))
+  const genres: string[] = Array.from(new Set(books.map((book) => book.genre)))
 
-function page () {
   return (
    <IndexClienPage books={books} genres={genres}/>
   )

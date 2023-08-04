a5bc33f43f5d1db7891d02bf99ce345beff1c88c
nator <gowluonade@gmail.com>
2023-08-03 20:15:15 -0500

    api-goncy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import { Book } from '@/types'
 import IndexClienPage from './client'
 
+const api = {
+  books: {
+    list: async (): Promise<Book[]> => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(import('../books.json').then((data) => data.library.map((data) => data.book)))
+        }, 3000)
+      })
+    }
+  }
+}
+
 async function page () {
   // const books: Book[] = await import('../books.json').then((data) => data.library.map((data) => data.book))
-  const books: Book[] = await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(import('../books.json').then((data) => data.library.map((data) => data.book)))
-    }, 3000)
-  })
+  const books = await api.books.list()
   const genres: string[] = Array.from(new Set(books.map((book) => book.genre)))
 
   return (
